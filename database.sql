e85562fbc6b884056e98dd4b298c6cb7bf8b17d6
Michael Ferlis <michaelferlis@gmail.com>
2019-06-21 15:51:38 -0500

    add full SQL file

--- database.sql
+++ database.sql
@@ -10,6 +10,12 @@ CREATE TABLE "genres" (
   "name" VARCHAR(80) NOT NULL
 );
 
+CREATE TABLE "movie_genres" (
+    "id" SERIAL PRIMARY KEY,
+    "movie_id" integer REFERENCES "movies" NOT NULL,
+    "genre_id" integer REFERENCES "genres" NOT NULL
+);
+
 INSERT INTO "movies" ("title", "poster", "description")
 VALUES 
 ('Avatar', 'images/avatar.jpeg', 'Avatar (marketed as James Cameron''s Avatar) is a 2009 American epic science fiction film directed, written, produced, and co-edited by James Cameron, and stars Sam Worthington, Zoe Saldana, Stephen Lang, Michelle Rodriguez, and Sigourney Weaver. The film is set in the mid-22nd century, when humans are colonizing Pandora, a lush habitable moon of a gas giant in the Alpha Centauri star system, in order to mine the mineral unobtanium, a room-temperature superconductor. The expansion of the mining colony threatens the continued existence of a local tribe of Na''vi – a humanoid species indigenous to Pandora. The film''s title refers to a genetically engineered Na''vi body operated from the brain of a remotely located human that is used to interact with the natives of Pandora.'),
