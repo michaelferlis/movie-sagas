2f887fa3e8d5c6564d968357b9324603d5b9fda6
Michael Ferlis <michaelferlis@gmail.com>
2019-06-21 16:14:42 -0500

    add working routes to DB

--- server/server.js
+++ server/server.js
@@ -2,13 +2,39 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
+const pool = require('./modules/pool');
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
 app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
-
+app.get('/movies', (req, res) => {
+    const queryText = 'SELECT * FROM "movies";'
+    pool.query(queryText)
+    .then(result => {
+        res.send(result.rows)
+    }).catch(
+        error => {
+            console.log('error with get movies route', error);
+            
+        }
+    )
+    
+})
+app.get('/genres', (req, res) => {
+    const queryText = 'SELECT * FROM "genres";'
+    pool.query(queryText)
+    .then(result => {
+        res.send(result.rows)
+    }).catch(
+        error => {
+            console.log('error with get genres route', error);
+            
+        }
+    )
+    
+})
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
