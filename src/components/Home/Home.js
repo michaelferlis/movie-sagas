367a559daf6b2f0bfef57372156c7adcd30f2496
Michael Ferlis <michaelferlis@gmail.com>
2019-06-23 13:16:09 -0500

    add new branch

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import './App.css';
 import {connect} from 'react-redux'
 import { thisTypeAnnotation } from '@babel/types';
+import Button from '@material-ui/core/Button';
 
 class Home extends Component {
   componentDidMount () {
@@ -12,16 +13,30 @@ class Home extends Component {
       type: 'FETCH_GENRES'
     })
   }
+
+  getDetails=(action)=> {
+    // this.props.dispatch({type: 'GET_DETAIL_PAGE',payload: this.props.movie})
+    this.props.history.push(`/details`)
+    console.log('testing');
+    
+  }
   // Renders the entire app on the DOM
   render() {
     return (
       <div className="App">
       {/* {JSON.stringify(this.props, null, 2)} */}
         <p>Home</p>
-        <ul>
+        <ul onclick={this.getDetails} >
+            
             {this.props.reduxState.movies.map(movie =>
-                <li key={movie.id}>{movie.title}</li>
+            
+                <li key={movie.id}><img onClick={this.getDetails} src={movie.poster}/>
+                <h2>{movie.title}</h2>{movie.description}<li></li>
+                <Button size="small" onClick={this.getDetails}>
+                            Details
+                        </Button></li>
                 )}
+            
         </ul>
       </div>
     );
